bcb4d331c8177bcb5abdc3eb5238ff5b6f87ee21
ArthurWang <yahui9119@gmail.com>
2024-07-24 14:52:05 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,13 @@
-FROM jenkins/jenkins:lts
+FROM jenkins/jenkins:2.452.3
 
 USER root
-
-RUN apt-get update && apt-get install -y libltdl7
+# RUN sed -i -E 's/(deb|security).debian.org/mirrors.aliyun.com/g' /etc/apt/sources.list
+RUN sed -i 's@deb.debian.org@mirrors.tuna.tsinghua.edu.cn@g' /etc/apt/sources.list.d/debian.sources
+RUN apt-get update && apt-get install -y libltdl7 bash
 
 ARG dockerGid=999
 
-RUN echo "docker:x:${dockerGid}:jenkins" >> /etc/group \
+RUN echo "docker:x:${dockerGid}:jenkins" >> /etc/group 
 
 COPY install.sh install.sh
 RUN bash install.sh
--- install.sh
+++ install.sh
@@ -1,18 +1,15 @@
-# Based on instructiions at https://www.microsoft.com/net/download/linux-package-manager/debian9/sdk-current
+# Based on instructiions at https://www.microsoft.com/net/download/linux-package-manager/debian11/sdk-current
 # Install dependency for .NET Core 2
 apt-get update
 apt-get install -y curl libunwind8 gettext apt-transport-https
 
-# Based on instructions at https://www.microsoft.com/net/download/linux-package-manager/debian9/sdk-current
+# Based on instructions at https://www.microsoft.com/net/download/linux-package-manager/debian11/sdk-current
 # Install microsoft.qpg & Install the .NET Core framework 
 
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
-mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
-wget -q https://packages.microsoft.com/config/debian/9/prod.list
-mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
-chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
-chown root:root /etc/apt/sources.list.d/microsoft-prod.list
+wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
 
 apt-get update
-apt-get install -y dotnet-sdk-2.2
+apt-get install -y dotnet-sdk-2.2 dotnet-sdk-8.0 dotnet-sdk-3.1 dotnet-sdk-6.0
 apt-get clean
